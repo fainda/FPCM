9350e1e2e4313e17319dd64dad42c2d820357657
Xman1109 <shawnoliver1109@gmail.com>
2025-01-03 16:12:58 +0100

    added the logic controller item and entity

---
+++ prototypes/items/logic_controller.lua
@@ -0,0 +1,13 @@
+data:extend({
+    {
+        type = "item",
+        name = "logic-controller",
+        icon = "__FPCM__/graphics/icons/items/logic_controller.png",
+        icon_size = 128,
+        icon_mipmaps = 4,
+        subgroup = "circuit-network",
+        order = "b[combinators]-c[logic-controller]",
+        place_result = "logic-controller",
+        stack_size = 1
+    }
+})
