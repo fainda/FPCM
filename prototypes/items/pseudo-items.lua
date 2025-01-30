0ce6a8bb945f8142ba92123c6dc3198bb3c6cf5b
Xman1109 <shawnoliver1109@gmail.com>
2025-01-30 17:36:50 +0100

    added pseudo items for ingame-wikis

---
+++ prototypes/items/pseudo-items.lua
@@ -0,0 +1,18 @@
+data:extend({
+    {
+        type = "item",
+        name = "pseudo-sensor",
+        icon = "__FPCM__/graphics/icons/items/pseudo-sensor.png",
+        icon_size = 64,
+        stack_size = 50,
+        flags = {"hidden"},
+    },
+    {
+        type = "item",
+        name = "pseudo-actuator",
+        icon = "__FPCM__/graphics/icons/items/pseudo-actuator.png",
+        icon_size = 64,
+        stack_size = 50,
+        flags = {"hidden"},
+    }
+})
